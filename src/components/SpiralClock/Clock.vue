ddeab62d24eb1f568d81a2d38422ea130f2a83e3
ginpei <ginpei@gmail.com>
2018-03-25 13:48:52 -0700

    componentization

--- src/components/SpiralClock/Clock.vue
+++ src/components/SpiralClock/Clock.vue
@@ -9,68 +9,72 @@
           Spiral(:settings="spiralSettings")
 </template>
 
-<script>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
 import Board from '@/components/SpiralClock/Board.vue';
 import Spiral from '@/components/SpiralClock/Spiral.vue';
 
-export default {
+@Component({
   components: {
     Board,
     Spiral,
   },
 
-  props: [
-    'now',
-    'length',
-    'longLength',
-  ],
-
-  computed: {
-    spiralSettings() {
-      const tzOffset = this.now.getTimezoneOffset();
-      // tz  |  deg
-      // -12 -> -360
-      //   0 ->    0
-      //  12 ->  360
-      const rotation = 270 + (tzOffset / 2);
-
-      return {
-        rotation,
-        radius: this.length,
-        gap: 20,
-        rounds: 2,
-        resolution: 60,
-      };
-    },
-
-    hourPath() {
-      const width = 10;
-      return `M 0 ${width} l 0 ${-this.length - width}`;
-    },
-
-    hourTransform() {
-      return `rotate(${this.hourRotation})`;
-    },
-
-    hourRotation() {
-      const hProgress = (this.now.getHours() % 12) / 12;
-      const mProgress = this.now.getMinutes() / 60;
-      const rotate = 360 * (hProgress + (mProgress / 12));
-      return rotate;
-    },
-
-    minutePath() {
-      const width = 10;
-      return `M 0 ${width} l 0 ${-this.longLength - width}`;
-    },
-
-    minuteTransform() {
-      const mProgress = this.now.getMinutes() / 60;
-      const rotate = 360 * mProgress;
-      return `rotate(${rotate})`;
-    },
+  props: {
+    now: Date,
+    length: Number,
+    longLength: Number,
   },
-};
+})
+export default class extends Vue {
+  now: Date = new Date(0);
+  length: number = 100;
+  longLength: number = 120;
+
+  get spiralSettings() {
+    const tzOffset = this.now.getTimezoneOffset();
+    // tz  |  deg
+    // -12 -> -360
+    //   0 ->    0
+    //  12 ->  360
+    const rotation = 270 + (tzOffset / 2);
+
+    return {
+      rotation,
+      radius: this.length,
+      gap: 20,
+      rounds: 2,
+      resolution: 60,
+    };
+  }
+
+  get hourPath() {
+    const width = 10;
+    return `M 0 ${width} l 0 ${-this.length - width}`;
+  }
+
+  get hourTransform() {
+    return `rotate(${this.hourRotation})`;
+  }
+
+  get hourRotation() {
+    const hProgress = (this.now.getHours() % 12) / 12;
+    const mProgress = this.now.getMinutes() / 60;
+    const rotate = 360 * (hProgress + (mProgress / 12));
+    return rotate;
+  }
+
+  get minutePath() {
+    const width = 10;
+    return `M 0 ${width} l 0 ${-this.longLength - width}`;
+  }
+
+  get minuteTransform() {
+    const mProgress = this.now.getMinutes() / 60;
+    const rotate = 360 * mProgress;
+    return `rotate(${rotate})`;
+  }
+}
 </script>
 
 <style lang="sass" scoped>
